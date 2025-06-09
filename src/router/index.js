dcf41af332cb6a097087b9d7d8af61962e3ff4e6
baitao <naimiaomiao520@gmail.com>
2025-06-09 19:00:27 +0800

    refactor(router): 注释掉 Demo 路由的组件引用

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
   {
     path: '/demo',
     name: 'Demo',
-    component: () => import('../components/PrintDataSelectorDemo.vue')
+    // component: () => import('../components/PrintDataSelectorDemo.vue')
   }
 ]
 
